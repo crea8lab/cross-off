b09e848ea2097adedc6ae53224fb25361cc1e0bd
Fayvor George <phavorsparks@gmail.com>
2019-02-22 12:25:16 +0100

    fix bug with deleting item

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,45 +1,46 @@
-const Todo = require('../models/todo')
+const Todo = require("../models/todo");
 
 module.exports = {
   home: (req, res) => {
-    res.redirect('/todos')
+    res.redirect("/todos");
   },
 
   getAll: async (req, res) => {
     try {
-      let todo = await Todo.find({}).sort({ date: -1 })
-      res.status(200)
-      res.render('todo', { todos: todo })
+      let todo = await Todo.find({}).sort({ date: -1 });
+      res.status(200);
+      res.render("todo", { todos: todo });
     } catch {
-      res.status(500)
+      res.status(500);
       res.json({
         success: false
-      })
+      });
     }
   },
 
   createTodo: async (req, res) => {
-    let { item } = req.body
+    let { item } = req.body;
 
     try {
       let data = await Todo({
         name: item
-      }).save()
-      return res.json(data)
+      }).save();
+      return res.json(data);
     } catch (error) {
-      res.status(409) 
-      return res.json({ success: false })
+      res.status(409);
+      return res.json({ success: false });
     }
   },
 
   deleteTodo: (req, res) => {
-    let item = req.params.item.replace(/\-/g, ' ')
+    let item = req.params.item.replace(/\-/g, " ");
     Todo.findOneAndRemove({ name: item })
-      .then((data) => {
-        return res.json(data)
+      .then(data => {
+        console.log(data);
+        return res.json(data);
       })
       .catch(err => {
-        res.json(err)
-      })
+        res.json(err);
+      });
   }
-}
+};
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,42 +1,40 @@
-$(document).ready(function(){
-
-  $('form').on('submit', function(){
-
-    const item = $('form input');
-    const todo = {item: item.val()};
+$(document).ready(function() {
+  $("form").on("submit", function() {
+    const item = $("form input");
+    const todo = { item: item.val() };
 
     $.ajax({
-      type: 'POST',
-      url: '/todos',
+      type: "POST",
+      url: "/todos",
       data: todo,
       timeout: 5000,
-      success: function(data){
+      success: function(data) {
         //do something with the data via front-end framework
         location.reload();
       },
-      fail: function (xhr, textStatus, thrownError) {
-        console.log('failed', textStatus)
+      fail: function(xhr, textStatus, thrownError) {
+        console.log("failed", textStatus);
       }
     });
 
     return false;
-
   });
 
-  $('li').on('click', function(){
-      const item = $(this).text().replace(/ /g, "-");
-      $.ajax({
-        type: 'DELETE',
-        url: '/todos/' + item,
-        timeout: 5000,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        },
-        fail: function(xhr, textStatus, thrownError) {
-          console.log(textStatus, thrownError)
-        }
-      });
+  $("li").on("click", function() {
+    const item = $("#item")
+      .text()
+      .replace(/ /g, "-");
+    $.ajax({
+      type: "DELETE",
+      url: "/todos/" + item,
+      timeout: 5000,
+      success: function(data) {
+        //do something with the data via front-end framework
+        location.reload();
+      },
+      fail: function(xhr, textStatus, thrownError) {
+        console.log(textStatus, thrownError);
+      }
+    });
   });
-
 });
